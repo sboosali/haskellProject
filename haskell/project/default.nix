35df3d23f9bccfb79abe20674a3917e0a360d6a5
Spiros Boosalis <SamBoosalis@gmail.com>
2018-07-05 13:47:10 -0700

    .

---
+++ haskell/project/default.nix
@@ -0,0 +1,37 @@
+{
+}:
+########################################
+let                                    # IMPORTS / UTILITIES
+
+
+
+in
+########################################
+let                                    # "PUBLIC" EXPORTS / "PRIVATE" HELPERS
+
+
+
+in
+########################################
+{                                      # THE EXPORTED "MODULE"
+
+
+
+}
+########################################
+/* NOTES
+
+====================
+
+TODO generate this object automatically from `cabal.project`.
+
+====================
+
+`haskellProject.project` adds `all = ...;` to its given object.
+
+====================
+
+====================
+
+*/
+########################################
